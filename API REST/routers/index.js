7b1c09963b9fa1fd4cf9f1275748f5fd1aa35ef8
Erick-4390 <71152113+Erick-4390@users.noreply.github.com>
2020-11-29 01:00:21 -0600

    Add files via upload

---
+++ API REST/routers/index.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+var bodyParser = require('body-parser');
+var request = require("request");
+
+var url = "https://itunes.apple.com/search?term="
+
+var url2 = ""
+
+var url3 = "http://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=GetListByName&name="
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+
+router.get('/', function (req, res) {
+    request({
+        url: url + "coldplay",
+        json: false
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            var contenido = JSON.parse(body);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(contenido));
+        } else {
+            res.status(500).send("Ocurrio un Error !!!");
+        }
+    })
+});
+
+module.exports = router;
